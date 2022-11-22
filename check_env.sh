c7585aae38d30ae354362ebf41193cd75a819893
danydavidov1 <davidovdaniel09@gmail.com>
2022-11-22 19:46:53 +0200

    add shell script for testing

--- check_env.sh
+++ check_env.sh
@@ -1,6 +1,6 @@
 req=$(curl -s -X GET http://localhost/container_list -H "Accept: application/json")
 echo "${req}"
 re='^[0-9]+$' 
-if ! [[ $req =~ $re ]] ; then                                                      
-   echo "error: Not a number" >&2;
+if ! [[ $req =~ $re ]] ; then 
+    echo "error: Not a number" >&2;
 fi
