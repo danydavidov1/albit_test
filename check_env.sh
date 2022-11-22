24018bd063adf4cdec709e2424562ee3a79859be
danydavidov1 <davidovdaniel09@gmail.com>
2022-11-22 20:18:27 +0200

    fix check_env

--- check_env.sh
+++ check_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+sleep 10
 get=$(curl -s -X GET http://localhost/container_list -H "Accept: application/json")
 echo "${get}"
 num='^[0-9]+$'
-if ! [[ $get =~ $num ]] ; then echo "error: Not a number" >&2; exit 1
+if ! [[ $get =~ $num ]] ; then 
+    echo "error: Not a number" >&2; exit 1
 fi
-echo "${get}"
-echo "${num}"
