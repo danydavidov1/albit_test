808d8d91a0d7575c80ccd3ea0d36afa11aa4a9ac
danydavidov1 <davidovdaniel09@gmail.com>
2022-11-22 19:28:17 +0200

    add shell script for testing

--- check_env.sh
+++ check_env.sh
@@ -1,4 +1,4 @@
-req=$(curl -s -X GET http://localhost/containr_list -H "Accept: application/json")
+req=$(curl -s -X GET http://localhost/container_list -H "Accept: application/json")
 echo "${req}"
 re='^[0-9]+$' 
 if ! [[ $req =~ $re ]] ; then                                                      
